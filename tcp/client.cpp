f71da00fe92ea37e4b859c9e42b2a8f33a6e77ac
nimbus154 <nimbus154@gmail.com>
2013-03-23 23:08:15 -0700

    Basic TCP server/client

--- tcp/client.cpp
+++ tcp/client.cpp
@@ -1,9 +1,77 @@
+/* client.cpp
+ * File transfer client, TCP
+ * Chad Wyszynski
+ * CPSC 471
+ *
+ * General usage:
+ * ./client <SERVER IP> <SERVER PORT> <FILE NAME>
+ *
+ * Example:
+ * ./client 127.0.0.1 1234 file.txt
+ *
+ * Operations:
+ * Establish a connection to the server.
+ *
+ * Send the name of the file to the server.
+ *
+ * Use the same connection to transfer the file contents to the server.
+ *
+ * Close the connection and exit.
+ */
 #include <sys/socket.h>
 #include <iostream>
+#include <string>
+#include <cstring>
 #include <fstream>
+#include <arpa/inet.h>
 
 using namespace std;
 
-int main() {
+/**
+ * Display directions for how to use the program
+ */
+void usage() {
+	cout << "This is a simple TCP client for transferring files.\n"
+		<< "To transfer files: \n\n"
+		<< "\tclient <IP> <port> <file to transfer>\n\n"
+		<< "For example:\n"
+		<< "\tclient 127.0.0.1 1234 file.txt\n";
+}
+
+int main(int argc, char** argv) {
+
+	if(argc != 4) {
+		usage();
+		return 0;
+	}
+
+
+	// TODO check all for failure
+
+	// create a socket with socket()
+	// check for non-negative
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// specify server IP and port
+	struct sockaddr_in server_addr;
+
+	// clear address structure
+	memset(&server_addr, 0, sizeof(server_addr));
+	
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(1234);
+
+	// set IP
+	// check for > 0 return value
+	inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+	// connect
+	// returns -1 on error, 0 on success
+	connect(sock_fd, (sockaddr*)&server_addr, sizeof(sockaddr));
+
+	write(sock_fd, "yo\0", 3);
+	
+	// transfer size of object, then object
+	// e.g. size of filename, then filename
 	return 0;
 }
--- tcp/server.cpp
+++ tcp/server.cpp
@@ -1,9 +1,89 @@
+/* server.cpp
+ * File transfer server, TCP
+ * Chad Wyszynski
+ * CPSC 471
+ *
+ * Usage:
+ * ./server <LISTENING PORT>
+ *
+ * Example:
+ * ./server 1234
+ *
+ * Listen for incoming connections on the specified port.
+ *
+ * When the client tries to connect, the server shall accept the connection.
+ *
+ * Use the connection created in part 2 to receive the name of the file from the
+ * client.
+ *
+ * Create the file with the name received in step 3.
+ *
+ * Receive the file contents from the client and write the contents to the file
+ * created in step 4.
+ *
+ * Close the connection and go back to waiting for more connections.
+ */
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
 #include <fstream>
 
 using namespace std;
 
-int main() {
+/**
+ * Display directions for how to use the program
+ */
+void usage() {
+	cout << "This is a simple TCP server for transferring files.\n"
+		<< "To listen: \n\n"
+		<< "\tserver <port>\n\n"
+		<< "For example:\n"
+		<< "\tserver 1234\n";
+}
+
+int main(int argc, char** argv) {
+	if(argc != 2) {
+		usage();
+		return 0;
+	}
+
+	// Expect to receive size of object, then read that number of bytes
+	struct sockaddr_in client_addr, server_addr;
+	socklen_t client_len = sizeof(client_addr);
+
+
+	// check for failure
+	int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	// clear address structure
+	memset(&server_addr, 0, sizeof(server_addr));
+	
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(1234);
+
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// check for value >= 0
+	bind(listen_fd, (sockaddr *) &server_addr, sizeof(server_addr));
+
+	listen(listen_fd, 100); // should be >= 0, 100 pending clients
+
+	// connection loop
+	char buffer[10];
+	while(true) {
+
+		int connection = accept(listen_fd, 
+								(sockaddr *)&client_addr, 
+								&client_len);
+
+		read(connection, buffer, 3);
+		printf("%s\n", buffer);
+
+		close(connection);
+	}
+
 	return 0;
 }
